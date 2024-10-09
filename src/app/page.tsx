8e2b49ae85c4045e898709ff5f70aeedb296f5c3
pool_1541 <pool_1541@hotmail.com>
2024-10-08 22:55:22 -0500

    feat: add new components on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import { About, Hero } from "@/components/pages/home";
-
+import { About, Contact, Hero, Services } from "@/components/pages/home";
+import { WhatsappButton } from "@/components/ui/whatsapp-button";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
+      <Services/>
+      <Contact />
+      <WhatsappButton />
     </>
   );
 }
