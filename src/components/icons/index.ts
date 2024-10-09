140b5283cefbaf9f3d63c6da496d3f89b6980a64
pool_1541 <pool_1541@hotmail.com>
2024-10-08 22:34:28 -0500

    chore: add new icons

---
+++ src/components/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as CheckIcon } from './check.icon';
+export { default as WhatsappIcon } from './whatsapp.icon';
+export { default as FacebookIcon } from './facebook.icon';
+export { default as InstagramIcon } from './instagram.icon';
