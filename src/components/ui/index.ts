43a510751f9f28ad33737e1c0da38f63cc6498b1
pool_1541 <pool_1541@hotmail.com>
2024-10-13 18:03:08 -0500

    chore: create barrel to ui components folder

---
+++ src/components/ui/index.ts
@@ -0,0 +1,6 @@
+export * from './blurry';
+export * from './burger-button';
+export * from './image-tiles';
+export * from './input-group';
+export * from './tiles';
+export * from './whatsapp-button';
