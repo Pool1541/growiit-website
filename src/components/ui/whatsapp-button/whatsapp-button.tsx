a74ec008f98697c1ec03c082d96d911aacd6a036
pool_1541 <pool_1541@hotmail.com>
2024-10-27 18:43:14 -0500

    chore: update whatsapp phone number

--- src/components/ui/whatsapp-button/whatsapp-button.tsx
+++ src/components/ui/whatsapp-button/whatsapp-button.tsx
@@ -3,7 +3,7 @@ import styles from './whatsapp-button.module.css'
 
 export default function WhatsappButton() {
   return (
-    <a className={styles['whatsapp-button']} href='https://api.whatsapp.com/send?phone=57311 5040128&text=Hola,%20deseo%20más%20información.'
+    <a className={styles['whatsapp-button']} href='https://api.whatsapp.com/send?phone=51932088984&text=Hola,%20deseo%20más%20información.'
     target='_blank'
     rel='noopener noreferrer'>
       <WhatsappIcon />
