6a8bea08e3134114a619d98cdc9ee12bb266a73a
pool_1541 <pool_1541@hotmail.com>
2024-11-03 23:02:54 -0500

    chore: create HorizontalCard component

---
+++ src/components/pages/blog/horizontal-card/horizontal-card.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable @next/next/no-img-element */
+import styles from './horizontal-card.module.css';
+import { MetadataLabel, Tag } from '@/components/ui';
+import { AvatarIcon, CalendarIcon } from '@/components/icons';
+
+export default function HorizontalCard({ title, description, image, author, date, tags = [], slug }: BlogArticleInterface) {
+  const articleLink = `/blog/${slug}`;
+
+  return (
+    <article className={styles['horizontal-card']}>
+      <div className={styles['image-container']}>
+        <a href={articleLink}>
+          <img src={image.src} alt={image.alt} />
+        </a>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.metadata}>
+          <MetadataLabel icon={<AvatarIcon />} size='medium'>
+            { author }
+          </MetadataLabel>
+          <MetadataLabel icon={<CalendarIcon />} size='medium'>
+            { date }
+          </MetadataLabel>
+        </div>
+        <a href={articleLink} target="_blank" rel="noopener noreferrer">
+          <h3>{ title }</h3>
+        </a>
+        <p>
+          { description }
+        </p>
+        <div className={styles.tags}>
+          {
+            tags.map(({ tag, slug }) => (
+              <Tag link={`/blog/tag/${slug}`} key={slug}>{ tag }</Tag>
+            ))
+          }
+        </div>
+      </div>
+    </article>
+  )
+}
