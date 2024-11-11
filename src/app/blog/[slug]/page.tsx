f0ffadc57c04e04f4a59340e9cfc7c8c688a7f29
pool_1541 <pool_1541@hotmail.com>
2024-11-11 18:20:59 -0500

    refactor: add blog page metadata generation

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,30 +1,44 @@
-import { Hero } from "@/components/pages/blog";
-import { notFound } from "next/navigation";
-import styles from './article.module.css'
-import { adjustColorBrightness, GenerateCssColorVariable } from "@/lib";
-import { Header } from "@/components/layout";
-import { HeaderVariables } from "@/types";
-import { Article } from "@/components/pages/blog/article";
-import getPosts, { getPost } from "@/lib/get-posts";
-import { PostBody } from "@/components/pages/blog/post-body/post-body";
-import { RecommendedArticles } from "@/components/pages/blog/recommended-articles";
+import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
+import { Header } from '@/components/layout';
+import { Article, Hero, PostBody, RecommendedArticles } from '@/components/pages/blog';
+import { adjustColorBrightness, GenerateCssColorVariable} from '@/lib';
+import getPosts, { getPost } from '@/lib/get-posts';
+import type { HeaderVariables } from '@/types';
+import styles from './article.module.css';
 
 type Params = Promise<{ slug: string }>;
 
+export const generateMetadata = async (props: { params: Params }): Promise<Metadata> => {
+  const params = await props.params;
+  const slug = await params.slug;
+  const posts = await getPosts();
+  const post = posts.find((post) => post.slug === slug);
+
+  return {
+    title: post?.title,
+    description: post?.description,
+    alternates: {
+      canonical: `https://growiit.com/blog/${params.slug}`,
+    },
+  };
+};
+
 export async function generateStaticParams() {
   const articles = await getPosts();
-  return articles.map(article => ({ slug: article.slug }));
+  return articles.map((article) => ({ slug: article.slug }));
 }
 
-export default async function Page(props: { params: Params}) {
+export default async function Page(props: { params: Params }) {
   const params = await props.params;
   const slug = await params.slug;
   const article = await getPost(slug);
 
   if (!article) return notFound();
-  const withBgColor = { "--custom-article-bg-color": article.color }
+  
+  const withBgColor = { '--custom-article-bg-color': article.color };
   const withDarkColor = GenerateCssColorVariable('--custom-article-dark-color', article.color);
-  const style = {...withBgColor, ...withDarkColor}
+  const style = { ...withBgColor, ...withDarkColor };
 
   const headerVariables: HeaderVariables = {
     '--text-color': '#000000',
@@ -38,10 +52,10 @@ export default async function Page(props: { params: Params}) {
       <main className={styles.article} style={style}>
         <Hero {...article} className={styles['pb-15']} noAnchors />
         <Article>
-          <PostBody>{ article.body }</PostBody>
+          <PostBody>{article.body}</PostBody>
         </Article>
         <RecommendedArticles />
       </main>
     </>
-  )
+  );
 }
