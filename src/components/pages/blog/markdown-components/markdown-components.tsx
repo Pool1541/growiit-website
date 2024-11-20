b4e9eee2324710ec8c654bea1ca1a75448ab8efd
pool_1541 <pool_1541@hotmail.com>
2024-11-19 23:22:04 -0500

    refactor: enhance paragraph component to conditionally render images

--- src/components/pages/blog/markdown-components/markdown-components.tsx
+++ src/components/pages/blog/markdown-components/markdown-components.tsx
@@ -4,8 +4,7 @@ import Link from 'next/link';
 import Image, { ImageProps } from 'next/image';
 import { MDXComponents } from 'mdx/types';
 import { Code } from 'bright';
-import { ReactNode } from 'react';
-
+import React, { isValidElement, ReactNode } from 'react';
 import styles from './markdown-componets.module.css';
 
 export const mdxComponents: MDXComponents = {
@@ -34,6 +33,7 @@ export const mdxComponents: MDXComponents = {
       </Code>
     );
   },
+  hr: (props) => <hr className={styles.hr} {...props} />,
   ul: (props) => <ul className={styles.list} {...props} />,
   h1: (props) => <h1 className={styles.text} {...props} />,
   h2: (props) => <h2 className={styles.text} {...props} />,
@@ -41,5 +41,22 @@ export const mdxComponents: MDXComponents = {
   h4: (props) => <h4 className={styles.text} {...props} />,
   h5: (props) => <h5 className={styles.text} {...props} />,
   h6: (props) => <h6 className={styles.text} {...props} />,
-  p: (props) => <p className={styles.text} {...props} />,
+  p: ({ children, ...props }) => {
+    const childArray = React.Children.toArray(children);
+    const isImageParagraph = childArray.every(
+      (child) =>
+        isValidElement(child) &&
+        (typeof child.type === 'function' && child.type.name === 'img')
+    );
+
+    if (isImageParagraph) {
+      return <>{children}</>;
+    }
+
+    return (
+      <p className={styles.text} {...props}>
+        {children}
+      </p>
+    );
+  },
 };
