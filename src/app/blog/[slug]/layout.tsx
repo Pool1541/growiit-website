996b298093f9b7a3c9d620146ed87428d9a1e248
pool_1541 <pool_1541@hotmail.com>
2024-11-22 23:41:21 -0500

    refactor: enhance metadata generation for blog posts with Open Graph and Twitter card support

--- src/app/blog/[slug]/layout.tsx
+++ src/app/blog/[slug]/layout.tsx
@@ -11,12 +11,41 @@ export const generateMetadata = async (props: { params: Params }): Promise<Metad
   const posts = await getPosts();
   const post = posts.find((post) => post.slug === slug);
 
+  if (!post) return {}
+  
+  const ogImage = post.images.find((image) => image.type === 'og') || post?.images[0];
+
   return {
-    title: `${post?.title} | Growiit`,
-    description: post?.description,
+    title: `${post.title} | Growiit`,
+    description: post.description,
     alternates: {
       canonical: `https://growiit.com/blog/${params.slug}`,
     },
+    twitter: {
+      title: post?.title,
+      description: post?.description,
+      card: "summary_large_image",
+      images: {
+        url: ogImage.src,
+        alt: ogImage.alt,
+        type: "image/webp",
+        width: 1200,
+        height: 630,
+      }
+    },
+    openGraph: {
+      type: "article",
+      title: post?.title,
+      description: post?.description,
+      url: `https://growiit.com/blog/${params.slug}`,
+      images: {
+        url: ogImage.src,
+        alt: ogImage.alt,
+        type: "image/webp",
+        width: 1200,
+        height: 630,
+      }
+    }
   };
 };
 
